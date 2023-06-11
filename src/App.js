998602bca2861e9704f09858b280847213a28ef7
bruno-connectabil <pamoroso.bruno@gmail.com>
2023-06-11 11:31:14 -0300

    Visual and some Functional Setup
    Positioned container centralized, added API to fetch quotes, created the whole skeleton

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import Quotebox from "./components/Quotebox";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+  const [rgb, setRgb] = useState([]);
+
+  function randomColor(){
+    const maxValue = 256;
+    let colorValue = 0;
+    let i = 0;
+
+    while(i < 3){
+      colorValue = Math.floor(Math.random() * maxValue);
+      if(i == 0){
+        setRgb(colorValue);
+      }
+      i++;
+    }
+    console.log(rgb);
+  }
+
+  useEffect(() => {
+    fetch("https://api.quotable.io/random", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setQuote('" ' + data.content + ' "');
+        setAuthor("— "+ data.author);
+        randomColor();
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{display: 'flex', justifyContent:'center', alignItems: 'center', width: '100vw', height: '100vh'}}>
+      <Quotebox quote={quote} author={author}></Quotebox>
     </div>
   );
 }
---
+++ src/components/Quotebox.js
@@ -0,0 +1,19 @@
+import styles from "./Quotebox.module.css";
+
+function Quotebox({ quote, author }) {
+  return (
+    <div id="quote-box" className={styles.quoteContainer}>
+      <div id="quote-container">
+        <p id="text" className={styles.quoteText}>{quote}</p>
+        <span id="author" className={styles.quoteAuthor}> {author}</span>
+      </div>
+      <hr></hr>
+      <div>
+        <button id="new-quote">New Quote</button>
+        <a id="tweet-quote"></a>
+      </div>
+    </div>
+  );
+}
+
+export default Quotebox;
